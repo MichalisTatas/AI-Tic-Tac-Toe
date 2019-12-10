13ca64fd47a62e3e8a1212e7c1349c2475736069
MichalisTatas <tatas.michalis@gmail.com>
2019-12-10 16:24:15 +0200

    alphaBeta not returning best move

--- source.py
+++ source.py
@@ -7,12 +7,13 @@ def getSuccessorStates(myBoard, player):
     else:
         symbol = 'O'
     successorStates = []
+    tempBoard = myBoard.copy()
     i=0
     while i<9:
-        if myBoard[i] == ' ':
-            myBoard[i] = symbol
-            successorStates.append(myBoard)
-            myBoard[i] = ' '
+        if tempBoard[i] == ' ': 
+            tempBoard[i] = symbol
+            successorStates.append(tempBoard)
+            tempBoard=myBoard.copy()
         i = i+1
     return successorStates
 
@@ -21,9 +22,6 @@ def isDrawTest(b):
     if (b[0] != ' ' and b[1] != ' ' and b[2] != ' ' and
         b[3] != ' ' and b[4] != ' ' and b[5] != ' ' and
         b[6] != ' ' and b[7] != ' ' and b[8] != ' '):
-        disableButton()
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", "It is a Tie! ")
-        board.quit()
         return True
     return False
 
@@ -54,7 +52,7 @@ def isWinForAiTest(b):
         b[0] == 'O' and b[3] == 'O' and b[6] == 'O' or
         b[1] == 'O' and b[4] == 'O' and b[7] == 'O' or
         b[6] == 'O' and b[5] == 'O' and b[8] == 'O') :
-        disableButton()
+        # disableButton()
         return True
     return False
 
@@ -64,17 +62,17 @@ def isWinForAiTest(b):
 
 def MiniMax(myBoard, depth, player, a, b):
     if isWinForAiTest(myBoard):
-        print("DADA")
+        # print("DADA")
         return 10
     if isWinForPlayerTest(myBoard):
-        print("MAMA")
+        # print("MAMA")
         return -10
     if isDrawTest(myBoard):
-        print("FUCKBOY")
+        # print("FUCKBOY")
         return 0
-    if depth == 5 :
-        print("CARUSO")
-        return 5
+    # if depth == 0 :
+    #     # print("CARUSO")
+    #     return 5
 
     if player == 'human' :
         maxEvaluation = -float("inf")
@@ -107,18 +105,54 @@ def aiTurn(depth):
     a = -float("inf")
     b = float("inf")
     bestScore = -inf
-
-    for successorState in getSuccessorStates(currentBoard, 'human'):
-        score = MiniMax(successorState, depth, 'ai', a, b)
+    getSuccessorStates(currentBoard, 'ai')
+    for successorState in getSuccessorStates(currentBoard, 'ai'):
+        # print(successorState)
+        p = successorState.copy()
+        score = MiniMax(p, depth, 'human', a, b)
         if score > bestScore:
             bestScore = score
-            bestB = successorState
+            bestB = successorState.copy()
         a = max(a, bestScore)
     i=0
     while i<9:
-        print("MPAMPAS")
         if(currentBoard[i] != bestB[i]):
-            currentBoard[i] = 'O'
+            if i==0:
+                button1['text'] = 'O'
+                board.update()
+                break
+            if i==1:
+                button2['text'] = 'O'
+                board.update()
+                break
+            if i==2:
+                button3['text'] = 'O'
+                board.update()
+                break
+            if i==3:
+                button4['text'] = 'O'
+                board.update()
+                break
+            if i==4:
+                button5['text'] = 'O'
+                board.update()
+                break
+            if i==5:
+                button6['text'] = 'O'
+                board.update()
+                break
+            if i==6:
+                button7['text'] = 'O'
+                board.update()
+                break
+            if i==7:
+                button8['text'] = 'O'
+                board.update()
+                break
+            if i==8:
+                button9['text'] = 'O'
+                board.update()
+                break
         i = i+1
 
     
